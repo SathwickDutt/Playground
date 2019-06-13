6c83623f519ea2166a70335b8d81aba772fa535f
SathwickDutt <51468423+SathwickDutt@users.noreply.github.com>
2019-06-13 11:56:43 +0530

    Test code pushed to repository.

---
+++ Circumference of a circle/Main.java
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+int main()
+{
+    float radius, circumference;
+    scanf("%f", &radius);
+
+    
+    /*
+     * Input radius of circle from user
+     */
+  
+    /*
+     * Calculate diameter, circumference and area
+     */
+    
+    circumference = 3.14 * radius * radius;
+    
+
+
+    printf(" %.2f ", circumference);
+
+
+    return 0;
+}
